d74be3e4acf7fab96f366aa96be1b3c8086f7eb5
Buwa <bubay565@yahoo.com>
2017-07-06 07:17:51 +0100

    added proptypes to components

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     
     render() {
         return (
-          <div><ListContacts onDeleteContact ={this.removeContact} contacts={this.state.contacts}/></div>
+          <div><ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/></div>
         );
       }
 }
